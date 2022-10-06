1861eb852e0f5b2a898e40b1928831335ac6f39d
Alexander R. Torrijos <artorrijos@gmail.com>
2022-10-06 18:26:31 +0800

    Added passing tests

--- sum.spec.js
+++ sum.spec.js
@@ -4,3 +4,6 @@ test('adds 1 + 2 to equal 3', () => {
   expect(sum(1, 2)).toBe(3);
 });
 
+test('adds hello + world to equal 0helloworld', () => {
+  expect(sum('hello', 'world')).toBe('0helloworld');
+});
