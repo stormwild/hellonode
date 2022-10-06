910e98dd7a38d4f4d43dd0a63de771f486f08230
Alexander R. Torrijos <artorrijos@gmail.com>
2022-10-06 18:53:14 +0800

    Updated tests

--- sum.spec.js
+++ sum.spec.js
@@ -7,3 +7,7 @@ test('adds 1 + 2 to equal 3', () => {
 test('adds hello + world to equal 0helloworld', () => {
   expect(sum('hello', 'world')).toBe('0helloworld');
 });
+
+test('adds hello + world to equal 0hello', () => {
+  expect(sum(...'hello')).toBe('0hello');
+});
