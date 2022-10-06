dc62407dab1efacc0ec3fea0fc1e2f6f05df85ef
Alexander R. Torrijos <artorrijos@gmail.com>
2022-10-06 18:03:13 +0800

    Example of rest and spread

--- index.js
+++ index.js
@@ -4,3 +4,23 @@ fs.writeFile('helloworld.txt', 'Hello World', (err) => { if (err) throw err; con
 
 console.log("Hello world")
 
+function sum(...theArgs) {
+    console.log(theArgs.length)
+    console.log(theArgs)
+  let total = 0;
+  for (const arg of theArgs) {
+    total += arg;
+  }
+  return total;
+}
+
+
+sum(1, 2, 3)
+
+sum(1, 2, 3, 4)
+
+sum('hello') // [ 'hello' ] length 1
+
+sum('hello', 'world') // [ 'hello', 'world' ] length 2
+
+sum(...'hello') // [ 'h', 'e', 'l', 'l', 'o' ] length 5
